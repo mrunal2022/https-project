45fec0575b2baf10004faf6e51804ebf24728780
mrunal2022 <mrunalghodke2022@gmail.com>
2021-11-29 20:03:55 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+@Injectable({ providedIn: 'root' })
 export class AppComponent {
   title = 'practise';
+  /*Email: string = "";
+  Password: string = "";*/
+
+  loginForm = new FormGroup({
+    'email': new FormControl("", Validators.required),
+    'password': new FormControl("", Validators.required)
+
+
+  });
+
+
+
+  constructor(public http: HttpClient) {
+
+  }
+  onSubmit() {
+
+    const values = this.loginForm.getRawValue();
+    this.http.post('https://login-page-angular-618a3-default-rtdb.firebaseio.com/users.json',
+      values).subscribe(response => {
+
+        console.log(response);
+      });
+
+
+
+
+  }
 }
